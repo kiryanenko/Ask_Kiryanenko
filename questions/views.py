0d57827c18381f5cbca41b84ce6f7b76a5d9427a
Kiryanenko <kiryanenkoav@mail.ru>
2017-04-04 02:40:05 +0300

    Create question page

--- questions/views.py
+++ questions/views.py
@@ -12,3 +12,22 @@ def ask(request):
     return render(request, 'questions/ask.html', {
 
     })
+
+def question(request):
+    q = {
+        'title': 'Заголовок вопроса?',
+        'text': 'Some quick example text to build on the card title and make up the bulk of the card. Example. ' * 3,
+        'rating': 123
+    }
+    tags = ['Some', 'quick', 'example']
+    answers = [{
+        'text': 'Some quick example text to build on the card title and make up the bulk of the card. Example. ' * 3,
+        'rating': 123
+    }, ] * 15
+
+    return render(request, 'questions/question.html', {
+        'question': q,
+        'tags': tags,
+        'answers': answers,
+    })
+
